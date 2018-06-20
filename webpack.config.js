a0f90c0de5981cd566bb7cdab489ff34f6d9509b
wenjie.xu <wenjie.xu@bianlifeng.com>
2018-06-20 21:45:24 +0800

    配置eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: __dirname,
-    filename: './release/bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /.js?$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
-}
+	entry: './src/index.js',
+	output: {
+		path: __dirname,
+		filename: './release/bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /.js?$/,
+				exclude: /(node_modules)/,
+				loader: 'babel-loader'
+			}
+		]
+	}
+};
