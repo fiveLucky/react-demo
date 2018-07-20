be480d8a9dd4af96885dae777209dda4b49b7789
wenjie.xu <wenjie.xu@bianlifeng.com>
2018-07-20 17:00:34 +0800

    增加热更新，样式解析

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 
 module.exports = {
 	entry: {
@@ -12,24 +14,35 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.js$/,
+				test: /\.jsx?$/,
 				exclude: /(node_modules)/,
 				loader: 'babel-loader'
 			},
 			{
 				test: /\.html$/,
 				loader: 'html-loader'
-			}
+			},
+			{
+				test: /\.less$/,
+				loader: 'style-loader!css-loader!less-loader'
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader?{"modules":true}'
+			},
+
 		],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: 'template/index.html'
-		})
+		}),
+		new webpack.HotModuleReplacementPlugin()
 	],
+	devtool: 'source-map',
 	devServer: {
 		contentBase: path.join(__dirname, 'release'),
-		compress: true,  // gzip
-		port: 9000
+		port: 9000,
+		hotOnly: true
 	}
 };
