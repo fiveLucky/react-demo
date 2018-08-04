a326ec345c4fcdb186d219818f485d39c4c57c65
徐文杰 <xuwenjie@xuwenjies-MacBook-Pro.local>
2018-08-04 18:19:33 +0800

    更新

--- src/App/List/index.js
+++ src/App/List/index.js
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 import style from './index.less';
 export default class Detail extends Component {
 	componentDidMount() {
-		// alert('this is list page');
+		this.getTitle().then(() => {
+			console.log('then');
+		}, (err) => {
+			console.log(err);
+		}).catch(err => {
+			console.log(err);
+		});
+		const arr = [1, 2, 3];
+		if (arr.includes(1)) {
+			console.log(1);
+		}
+	}
+	async getTitle() {
+		throw new Error('I am a error');
 	}
 	render() {
 		return (
