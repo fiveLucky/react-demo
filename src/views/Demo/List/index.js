833b8596cd9e836a92b1a5203a84462586269d28
wenjie.xu <wenjie.xu@bianlifeng.com>
2018-12-19 11:04:47 +0800

    调整渲染

--- src/views/Demo/List/index.js
+++ src/views/Demo/List/index.js
@@ -6,26 +6,30 @@ import { observer } from 'mobx-react';
 import store from './store';
 import styles from './index.less';
 
-
+const genderEnum = {
+  male: '男',
+  female: '女'
+};
 const columns = [
   {
     title: '姓名',
     dataIndex: 'name',
   },
   {
-    title: '年龄',
+    title: '年龄（岁）',
     dataIndex: 'age',
   },
   {
     title: '性别',
     dataIndex: 'gender',
+    render: text => (<span>{genderEnum[text]}</span>)
   },
   {
-    title: '身高',
+    title: '身高（cm）',
     dataIndex: 'height',
   },
   {
-    title: '体重',
+    title: '体重（公斤）',
     dataIndex: 'weight',
   },
 ];
