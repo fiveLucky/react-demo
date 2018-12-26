a5f73f7d215c8c3b63eef71e8895901bf1ed5a5b
wenjie.xu <wenjie.xu@bianlifeng.com>
2018-12-26 15:56:13 +0800

    抽离base config文件

--- Config/webpackConfig/dev.js
+++ Config/webpackConfig/dev.js
@@ -1,61 +1,9 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { outputPath, publicPath } = require('../../project.config.js').output;
-const { spliceRootPath, spliceDirPath } = require('../util');
+const webpackMerge = require('webpack-merge');
+const { outputPath } = require('../../project.config.js').output;
+const { spliceRootPath } = require('../util');
+const baseConfig = require('./base');
 
-module.exports = {
-	mode: 'development',
-	entry: ['./src/index.js',],
-	output: {
-		path: spliceRootPath(outputPath),
-		publicPath,
-		filename: '[name]-bundle.js',
-		chunkFilename: '[name]@[chunkhash].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				exclude: /(node_modules)/,
-				use: [
-					'babel-loader',
-					spliceDirPath(__dirname, 'lazyLoader.js')
-				]
-			},
-			{
-				test: /\.html$/,
-				loader: 'html-loader'
-			},
-			{
-				test: /\.less$/,
-				use: [
-					"style-loader",
-					{
-						loader: 'css-loader',
-						options: {
-							modules: true,
-							localIdentName: '[name]_[local]-[hash:base64:5]'
-						}
-					},
-					"less-loader",
-				],
-				exclude: /style/
-			},
-			{
-				test: /\.less$/,
-				loader: 'style-loader!css-loader!less-loader',
-				include: /style/,
-			},
-			{
-				test: /\.css$/,
-				loader: 'style-loader!css-loader?{"modules":true}'
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'template/index.html'
-		}),
-	],
+const devConfig = {
 	devtool: 'inline-source-map',
 	devServer: {
 		contentBase: spliceRootPath(outputPath),
@@ -63,3 +11,4 @@ module.exports = {
 		open: true
 	}
 };
+module.exports = webpackMerge.smart(baseConfig, devConfig);
--- Config/webpackConfig/prod.js
+++ Config/webpackConfig/prod.js
@@ -1,36 +1,10 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
-const { spliceRootPath, spliceDirPath } = require('../util');
-const { outputPath, publicPath } = require('../../project.config.js').output;
+const webpackMerge = require('webpack-merge');
+const baseConfig = require('./base');
 
 
-
-const config = {
-  mode: 'production',
-  entry: {
-    index: './src/index.js',
-    vendors: [
-      'react',
-      'react-dom',
-      'react-router',
-      'moment',
-      'mobx',
-      'mobx-react',
-      'rc-menu',
-      'rc-notification',
-      'rc-tooltip',
-      'rc-select',
-      'rc-table',
-    ]
-  },
-  output: {
-    path: spliceRootPath(outputPath),
-    publicPath,
-    filename: '[name]@[chunkhash].js',
-    chunkFilename: '[name]@[chunkhash].js'
-  },
+const prodConfig = {
   optimization: {
     splitChunks: {
       chunks: 'async',
@@ -56,60 +30,16 @@ const config = {
     }
 
   },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: [
-          'babel-loader',
-          spliceDirPath(__dirname, 'lazyLoader.js')
-        ]
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader'
-      },
-      {
-        test: /\.less$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName: '[name]_[local]-[hash:base64:5]'
-            }
-          },
-          "less-loader",
-        ],
-        exclude: /style/
-      },
-      {
-        test: /\.less$/,
-        loader: 'style-loader!css-loader!less-loader',
-        include: /style/,
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader?{"modules":true}'
-      },
-    ],
-  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "style@[contenthash].css",
       chunkFilename: "style@[contenthash].css"
     }),
-    new HtmlWebpackPlugin({
-      template: 'template/index.html'
-    }),
     new ParallelUglifyPlugin({
       cacheDir: '.cache/',
     }),
-    new webpack.NoEmitOnErrorsPlugin(),
   ],
 };
 
 
-module.exports = config;
+module.exports = webpackMerge.smart(baseConfig, prodConfig);
