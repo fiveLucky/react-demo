aac613fac6e98212ada86f5c2d990a139da87026
wenjie.xu <wenjie.xu@bianlifeng.com>
2018-12-26 15:54:09 +0800

    更改analyze sercer port

--- Config/webpackConfig/analyze.js
+++ Config/webpackConfig/analyze.js
@@ -1,8 +1,10 @@
-const webpackConfig = require('./dev.js');
+const webpackConfig = require('./prod.js');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
 
-webpackConfig.plugins.push(new BundleAnalyzerPlugin());
+webpackConfig.plugins.push(new BundleAnalyzerPlugin({
+  analyzerPort: 8889
+}));
 const smp = new SpeedMeasurePlugin();
 
 module.exports = smp.wrap(webpackConfig);
