58fededfc6ea35dd82d4b80b43db6612cbeb5d8a
wenjie.xu <wenjie.xu@bianlifeng.com>
2019-04-30 17:54:39 +0800

    使用loadable懒加载

--- Config/webpackConfig/lazyLoader.js
+++ Config/webpackConfig/lazyLoader.js
@@ -1,24 +1,29 @@
 
-const reg = /.*lazy\(('(.*)')\).*/;
-const temp = (chunkName) => {
-  return `() => import(/* webpackChunkName: "${chunkName}" */ '${chunkName}')`;
+const reg = /.*import\s+(\S*)\s+from\s+['"]!lazy(\S*)['"]/;
+const importTemp = `
+  import Loadable from 'react-loadable';
+`;
+const lazyTemp = (chunkName, chunkPath) => {
+  return ` const ${chunkName} = Loadable({
+      loading: () => (<div>loading...</div>),
+      loader: () => import(/* webpackChunkName: "${chunkName}" */ '${chunkPath}')
+    }) `;
 };
+
 
 module.exports = (source) => {
   try {
     const r = source.split('\n').map(line => {
-      //eg: const List = lazy('./View/Demo/List');
       const matchArr = line.match(reg);
-
       if (matchArr !== null) {
-        const chunkName = matchArr[2];
-        const chunkPath = matchArr[1];
-        return line.replace(chunkPath, temp(chunkName));
+        const chunkName = matchArr[1];
+        const chunkPath = matchArr[2];
+        return line.replace(matchArr[0], lazyTemp(chunkName, chunkPath));
       }
       return line;
     }).join('\n');
 
-    return r;
+    return importTemp + r;
   } catch (e) {
     console.log(e);
   }
--- src/router.js
+++ src/router.js
@@ -1,25 +1,25 @@
 
-import React, { Suspense, lazy, Component } from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 import './style/component';
 import App from './App';
 
+import DemoList from '!lazy./views/Demo';
+import Home from '!lazy./views/Home';
 
-const DemoList = lazy('./views/Demo');
-const Home = lazy('./views/Home');
+
 
 export default class Router extends Component {
   render() {
     return (
       <BrowserRouter>
         <App>
-          <Suspense fallback={'loading'}>
-            <Switch>
-              <Route exact path={`/web`} component={Home}></Route>
-              <Route path={`/web/Demo`} component={DemoList}></Route>
-            </Switch>
-          </Suspense>
+          <Switch>
+            <Route exact path={`/web`} component={Home}></Route>
+            <Route path={`/web/Demo`} component={DemoList}></Route>
+          </Switch>
         </App>
       </BrowserRouter>
     );
