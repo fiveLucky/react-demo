5e1f83eb99e392853e9b6a2dc4fb6de57ae1ad0c
xuwenjie <fivelucky@126.com>
2020-09-24 11:36:52 +0800

    feat：动画尝测试

--- src/views/ReactUseDemo/useBattery/index.js
+++ src/views/ReactUseDemo/useBattery/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useBattery } from 'react-use';
 import styles from './index.less';
 
 export default function Index() {
   const batteryState = useBattery();
+  const [activeId, setActiveId] = useState(2);
   const {
     charging, // 是否在充电
     chargingTime, // 充满电时间
@@ -26,6 +27,12 @@ export default function Index() {
         )}
       </div>
       {charging && <div className={styles.batteryLightning}></div>}
-    </div>
+      <div className={styles.tabs}>
+        <div className={activeId === 1 && styles.active} onClick={() => setActiveId(1)}>one</div>
+        <div className={activeId === 2 && styles.active} onClick={() => setActiveId(2)}>two</div>
+        <div className={activeId === 3 && styles.active} onClick={() => setActiveId(3)}>three</div>
+        <div className={activeId === 4 && styles.active} onClick={() => setActiveId(4)}>four</div>
+      </div>
+    </div >
   );
 }
